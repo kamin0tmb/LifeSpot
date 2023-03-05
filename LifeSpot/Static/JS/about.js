c4c21a8f2d6776a91142f6f73232d822afd6b2fa
kamin0 <jm187@yandex.ru>
2023-03-06 00:22:30 +0300

    Добавьте файлы проекта.

---
+++ LifeSpot/Static/JS/about.js
@@ -0,0 +1,63 @@
+﻿//alert("Javascript для страницы 'о проекте' подключен!")
+
+/*
+* Запросим пользовательский ввод
+* и сохраним отзыв в объект
+*
+* */
+function getReview() {
+    // Создадим объект
+    let review = {}
+
+    // Сохраним свойство имени
+    review["userName"] = prompt("Как вас зовут ?")
+    if (review["userName"] == null) {
+        return
+    }
+
+    // Сохраним текст отзыва
+    review["comment"] = prompt("Напишите свой отзыв")
+    if (review["comment"] == null) {
+        return
+    }
+
+    // Сохраним текущее время
+    review["date"] = new Date().toLocaleString()
+
+    // Добавим на страницу
+    writeReview(review)
+}
+
+/*
+* Запишем отзыв на страницу
+*
+* */
+const writeReview = review => {
+    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
+        `<p>${review['comment']}</p>` +
+        '</div>';
+}
+
+/*Слайдер*/
+var indexValue = 1;
+showImg(indexValue);
+function btm_slide(num) { showImg(indexValue = num); }
+function side_slide(num) { showImg(indexValue += num); }
+function showImg(num) {
+    var i;
+    const img = document.querySelectorAll('img');
+    const slider = document.querySelectorAll('.btm-slides span');
+    if (num > img.length)
+        indexValue = 1;
+    if (num < 1)
+        indexValue = img.length;
+    for (i = 0; i < img.length; i++) {
+        img[i].style.display = "none";
+    }
+    for (i = 0; i < slider.length; i++) {
+        slider[i].style.background = "rgba(255,255,255,0.2)";
+    }
+    img[indexValue - 1].style.display = "block";
+    slider[indexValue - 1].style.background = "rgba(255,255,255,0.7)";
+}
---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,55 @@
+﻿/*
+* Функция для фильтрации контента
+* Будет вызываться благодаря атрибуту oninput на index.html
+*
+* */
+
+function filterContent() {
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i <= elements.length; i++) {
+        let videoText = elements[i].getElementsByTagName('h3')[0].innerText;
+
+        if (!videoText.toLowerCase().includes(inputParseFunction()/* Захват переменной теперь происходит с помощью замыкания */ .toLowerCase())) {
+            elements[i].style.display = 'none';
+        } else {
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
+
+/*
+* Сессия теперь создается в общей области видимости.
+* Будет "захватываться" тремя функциями
+* 
+* */
+let session = new Map();
+
+/*
+* Сохранение данных сессии сразу при заходе пользователя на страницу
+* 
+* */
+function handleSession() {
+    // Сохраним время начала сессии
+    session.set("startDate", new Date().toLocaleString())
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent)
+}
+
+/*
+* Вывод данных сессии в консоль
+* 
+* */
+let sessionLog = function logSession() {
+    for (let result of session) {
+        console.log(result)
+    }
+}
+
+/*
+* Всплывающее окно будет показано по таймауту
+* 
+* */
+setTimeout(() =>
+    alert("Нравится LifeSpot? " + '\n' + "Подпишитесь на наш телеграмм-канал t.me/life_spot"),
+    100000);
